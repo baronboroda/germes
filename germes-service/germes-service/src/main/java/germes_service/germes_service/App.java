581738cbb33546bc4206d1001a1f5a6a2853c1f5
Maksym Zelenin <maksym.zelenin@edvantis.com>
2018-01-15 17:33:49 +0200

    Service module was added

---
+++ germes-service/germes-service/src/main/java/germes_service/germes_service/App.java
@@ -0,0 +1,13 @@
+package germes_service.germes_service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
